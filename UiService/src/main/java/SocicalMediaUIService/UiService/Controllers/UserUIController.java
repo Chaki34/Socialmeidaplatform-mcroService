e1376d1c7de086e5da8a3d916edef7d02eb7edab
chkaki34 <debmalyachaki5@gmail.com>
2026-08-04 03:08:02 +0530

    ui update v3

--- UiService/src/main/java/SocicalMediaUIService/UiService/Controllers/UserUIController.java
+++ UiService/src/main/java/SocicalMediaUIService/UiService/Controllers/UserUIController.java
@@ -8,43 +8,40 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ui/users")
 @RequiredArgsConstructor
 public class UserUIController {
 
-
     private final UserFeignClient userFeignClient;
 
-
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(
-            @RequestBody CreateUserRequest request){
-
-        return ResponseEntity.ok(
-                userFeignClient.createUser(request)
-        );
+    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request){
+        return ResponseEntity.ok(userFeignClient.createUser(request));
     }
-
 
     @PutMapping("/{userUuid}/profile")
     public ResponseEntity<UserResponse> setupProfile(
             @PathVariable String userUuid,
             @RequestBody ProfileSetupRequest request){
-
-        return ResponseEntity.ok(
-                userFeignClient.setupProfile(userUuid,request)
-        );
+        return ResponseEntity.ok(userFeignClient.setupProfile(userUuid, request));
     }
 
+    @GetMapping("/{userUuid}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable String userUuid) {
+        return ResponseEntity.ok(userFeignClient.getUserByUuid(userUuid));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userFeignClient.getAllUsers());
+    }
 
     @DeleteMapping("/{userUuid}")
-    public ResponseEntity<Void> deleteUser(
-            @PathVariable String userUuid){
-
+    public ResponseEntity<Void> deleteUser(@PathVariable String userUuid){
         userFeignClient.deleteUser(userUuid);
-
         return ResponseEntity.noContent().build();
     }
-
 }
