b1564b8f465b92a6f346a413052936926602acb9
chkaki34 <debmalyachaki5@gmail.com>
2026-08-17 00:38:45 +0530

    add landing page

--- UiService/src/main/java/SocicalMediaUIService/UiService/Controllers/ViewController.java
+++ UiService/src/main/java/SocicalMediaUIService/UiService/Controllers/ViewController.java
@@ -3,6 +3,7 @@ package SocicalMediaUIService.UiService.Controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -16,6 +17,13 @@ public class ViewController {
     @GetMapping("/feed")
     public String userFeedPage() {
         return "userFeed";
+    }
+
+
+    @GetMapping("/")
+    public String LandingPage(Model model) {
+        model.addAttribute("heroTitle", "Your life's work, powered by our life's work");
+        return "landing";
     }
 
     @GetMapping("/login")
